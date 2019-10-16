e4d2a939f0d5d633848b2f76d3cfa1a68460a3ea
Rong Liu <rliu.pro@gmail.com>
2019-10-16 16:42:39 -0700

    change configurations (still not working)

--- hadoop-env.sh
+++ hadoop-env.sh
@@ -24,7 +24,7 @@
 # The java implementation to use.
 # export JAVA_HOME=/usr/
 # export JAVA_HOME=${JAVA_HOME}
-export JAVA_HOME=/usr/lib/jvm/default-java
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
 # The jsvc implementation to use. Jsvc is required to run secure datanodes
 # that bind to privileged ports to provide authentication of data transfer
--- start.sh
+++ start.sh
@@ -16,9 +16,10 @@ $HADOOP_HOME/bin/hdfs dfs -chmod -R 775 /user/hive
 $HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/hdpu
 $HADOOP_HOME/bin/hdfs dfs -chown -R hdpu:hdfs /user/hdpu
 $HADOOP_HOME/bin/hdfs dfs -chmod 755 /user/hdpu
-
+$HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/hive/tmp/sparkeventlog
+$HADOOP_HOME/bin/hdfs dfs -chmod -R 777 /user/hive/tmp
 $HADOOP_HOME/bin/hdfs dfs -mkdir /spark-jars
-$HADOOP_HOME/bin/hdfs put $SPARK_HOME/jars/* /spark-jars
+$HADOOP_HOME/bin/hdfs dfs -put $SPARK_HOME/jars/* /spark-jars
 
 # start mysql and create databases/users for hue and hive
 chown -R mysql:mysql /var/lib/mysql
